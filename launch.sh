915121816bd5e8dc6e4066c960c2f39fb0763f83
GSRMOHAMMAD <mamadbt19@gmail.com>
2016-07-02 12:29:53 -0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-[[ -n "$DEBUG" ]] && set -x
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
 
-# if [ "$(ls -A /usr/local/share/ca-certificates)" ]; then
-#     cat /usr/local/share/ca-certificates/* >> /etc/ssl/certs/ca-certificates.crt
-# fi
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.2.1 # Current stable
 
-/bin/mantl-api $@
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure --prefix=$PREFIX --sysconfdir=$PREFIX/luarocks --force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+install_rocks() {
+
+  ./.luarocks/bin/luarocks install luasec
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install luasocket
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+}
+
+if [ "$1" = "install" ]; then
+  install_luarocks
+  install_rocks
+else
+  while true; do
+    lua bot.lua
+    sleep 5s
+  done
+fi
